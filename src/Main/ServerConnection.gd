ecaf12de76e02961337f37d3e039d195182dcf9c
Agus Prayogi <agus21apy@gmail.com>
2022-03-10 21:38:14 +0700

    :hammer: modified

--- src/Main/ServerConnection.gd
+++ src/Main/ServerConnection.gd
@@ -1,3 +1,13 @@
 extends Node
 
 var key := "agus_nakama"
+var host := "127.0.0.1"
+var port := 7350
+var scheme = "http"
+var client : NakamaClient
+
+func _ready():
+	client = NakamaClient.create_client(key, host, port, scheme)
+
+func register_email(email: String, password: String):
+	yield(client.authenticate_email_async(email, password), "compleated")
