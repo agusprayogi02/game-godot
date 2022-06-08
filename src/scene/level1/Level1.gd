a6645881ad37e4a78ca28acb20ca7eb9c31fc915
Agus Prayogi <agus21apy@gmail.com>
2022-06-08 12:59:06 +0700

    :hammer: platform satu arah

--- src/scene/level1/Level1.gd
+++ src/scene/level1/Level1.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _on_ZonaJatuh_body_entered(body):
 	if body.name == 'Player':
-		get_tree().change_scene("res://src/scene/Level1.tscn")
+		get_tree().change_scene("res://src/scene/level1/Level1.tscn")
